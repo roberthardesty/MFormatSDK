f5b5f03789a015eb79e1a8321e07cb398b8bad60
roberth <robert.hardesty@javs.com>
2017-03-02 13:41:58 -0500

    Initial

---
+++ MFormatConfidence1/MFormatConfidence1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MFormatConfidence1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPreview = new System.Windows.Forms.Panel();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.btnPlayback = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelPreview
+            // 
+            this.panelPreview.Location = new System.Drawing.Point(12, 12);
+            this.panelPreview.Name = "panelPreview";
+            this.panelPreview.Size = new System.Drawing.Size(517, 238);
+            this.panelPreview.TabIndex = 0;
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(12, 256);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.TabIndex = 1;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayback
+            // 
+            this.btnPlayback.Location = new System.Drawing.Point(453, 256);
+            this.btnPlayback.Name = "btnPlayback";
+            this.btnPlayback.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayback.TabIndex = 2;
+            this.btnPlayback.Text = "Playback";
+            this.btnPlayback.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 415);
+            this.Controls.Add(this.btnPlayback);
+            this.Controls.Add(this.btnRecord);
+            this.Controls.Add(this.panelPreview);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPreview;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnPlayback;
+    }
+}
+
---
+++ MFormatConfidence1/MFormatConfidence1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MFORMATSLib;
+using System.Threading;
+
+namespace MFormatConfidence1
+{
+    public partial class Form1 : Form
+    {
+
+        private MFWriter Writer;
+        private MFReader Reader;
+        private MFPreview Preview;
+
+        private CancellationTokenSource cancelSource;
+        private Thread threadWorker;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       private void Form1_Load(object sender, EventArgs e)
+        {
+            Writer = new MFWriter();
+            Reader = new MFReader();
+            Preview = new MFPreview();
+
+            Preview.PreviewWindowSet("", panelPreview.Handle.ToInt32());
+            Preview.PreviewEnable("", 1, 1);
+
+            cancelSource = new CancellationTokenSource();
+            threadWorker = new Thread(() => thread_DoWork(cancelSource.Token));
+            threadWorker.Name = "thread_DoWork";
+            threadWorker.Start();
+
+        }
+
+        private void thread_DoWork(CancellationTokenSource cancelSource)
+        {
+
+        }
+    }
+}
