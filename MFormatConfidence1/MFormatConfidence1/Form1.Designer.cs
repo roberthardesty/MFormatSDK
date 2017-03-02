d3ba9dfddfa6afc605a9c1ca94b9573d1241c71b
roberth <robert.hardesty@javs.com>
2017-03-02 15:32:16 -0500

    lazy

--- MFormatConfidence1/MFormatConfidence1/Form1.Designer.cs
+++ MFormatConfidence1/MFormatConfidence1/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.panelPreview = new System.Windows.Forms.Panel();
             this.btnRecord = new System.Windows.Forms.Button();
             this.btnPlayback = new System.Windows.Forms.Button();
+            this.comboBoxDevices = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // panelPreview
@@ -58,11 +59,22 @@
             this.btnPlayback.Text = "Playback";
             this.btnPlayback.UseVisualStyleBackColor = true;
             // 
+            // comboBoxDevices
+            // 
+            this.comboBoxDevices.FormattingEnabled = true;
+            this.comboBoxDevices.Location = new System.Drawing.Point(93, 258);
+            this.comboBoxDevices.Name = "comboBoxDevices";
+            this.comboBoxDevices.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDevices.TabIndex = 3;
+            this.comboBoxDevices.Text = "Default";
+            this.comboBoxDevices.SelectedIndexChanged += new System.EventHandler(this.comboBoxDevices_SelectedIndexChange);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(541, 415);
+            this.Controls.Add(this.comboBoxDevices);
             this.Controls.Add(this.btnPlayback);
             this.Controls.Add(this.btnRecord);
             this.Controls.Add(this.panelPreview);
@@ -77,6 +89,7 @@
         private System.Windows.Forms.Panel panelPreview;
         private System.Windows.Forms.Button btnRecord;
         private System.Windows.Forms.Button btnPlayback;
+        private System.Windows.Forms.ComboBox comboBoxDevices;
     }
 }
 
--- MFormatConfidence1/MFormatConfidence1/Form1.cs
+++ MFormatConfidence1/MFormatConfidence1/Form1.cs
@@ -18,7 +18,13 @@ namespace MFormatConfidence1
 
         private MFWriter Writer;
         private MFReader Reader;
-        private MFPreview Preview;
+        private MFPreviewClass Preview;
+        private MFLiveClass Live;
+
+        M_AV_PROPS avProps;
+
+        private bool isWorking;
+        private bool isRec;
 
         private CancellationTokenSource cancelSource;
         private Thread threadWorker;
@@ -32,19 +38,135 @@ namespace MFormatConfidence1
         {
             Writer = new MFWriter();
             Reader = new MFReader();
-            Preview = new MFPreview();
+            Preview = new MFPreviewClass();
 
             Preview.PreviewWindowSet("", panelPreview.Handle.ToInt32());
             Preview.PreviewEnable("", 1, 1);
+
+            FillComboBox();
 
             cancelSource = new CancellationTokenSource();
             threadWorker = new Thread(() => thread_DoWork(cancelSource.Token));
             threadWorker.Name = "thread_DoWork";
             threadWorker.Start();
 
+            avProps.vidProps.eVideoFormat = eMVideoFormat.eMVF_Custom;
+
         }
 
-        private void thread_DoWork(CancellationTokenSource cancelSource)
+        private void thread_DoWork(CancellationToken cancelSource)
+        {
+            while (!cancelSource.IsCancellationRequested)
+            {
+                if (isWorking)
+                {
+                    MFFrame frame = null;
+                    Live.SourceFrameConvertedGet(ref avProps, -1, out frame, "");
+
+                    if(frame != null)
+                    {
+                        if (isRec)
+                        {
+                            btnRecord.Enabled = false;
+                            btnRecord.Text = "Recording";
+                            btnRecord.BackColor = Color.Red;
+
+                            try
+                            {
+                                Writer.ReceiverFramePut(frame, -1, "");
+
+                                //UpdateStatistic()
+                            }
+                            catch(Exception e)
+                            {
+                                MessageBox.Show("Error: " e.ToString());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        btnRecord.Enabled = true;
+                        btnRecord.Text = "Record";
+                        btnRecord.BackColor = Color.AntiqueWhite;
+                    }
+
+                    Preview.ReceiverFramePut(frame, -1, "");
+
+                    Marshal.ReleaseComObject(frame);                
+                }
+                else
+                {
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
+        private void FillComboBox()
+        {
+            comboBoxDevices.Items.Clear();
+
+            int deviceCount;
+            try
+            {
+                Live.DeviceGetCount(eMFDeviceType.eMFDT_Video, out deviceCount);
+
+                if(deviceCount > 0)
+                {
+                    for(int i =0; i<deviceCount; i++)
+                    {
+                        string strName;
+                        int isBusy;
+                        Live.DeviceGetByIndex(eMFDeviceType.eMFDT_Video, i, out strName, out isBusy);
+                        comboBoxDevices.Items.Add(strName);
+                    }
+
+                    comboBoxDevices.SelectedIndex = comboBoxDevices.Items.Count -1;
+                }
+
+                btnRecord.Enabled = false;
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void comboBoxDevices_SelectedIndexChange(object sender, EventArgs e)
+        {
+            //M_VID_PROPS vidProps;
+
+            Live.DeviceSet(eMFDeviceType.eMFDT_Video, comboBoxDevices.SelectedIndex, "");
+
+            ////RefreshProps
+
+            //int fCount, lCount;
+            //string name, options, help;
+
+            //Live.FormatVideoGetCount(eMFormatType.eMFT_Input, out fCount);
+            //if (fCount > 0)
+            //{
+            //    for(int i =0; i < fCount; i++)
+            //    {
+
+            //    }
+            //}
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            InitLive();
+            InitRecord();
+        }
+
+        private void InitLive()
+        {
+            Live.DeviceSet(eMFDeviceType.eMFDT_Video, comboBoxDevices.SelectedIndex, "");
+            M_VID_PROPS vidProps;
+            string name;
+            if()
+        }
+
+        private void InitRecord()
         {
 
         }
