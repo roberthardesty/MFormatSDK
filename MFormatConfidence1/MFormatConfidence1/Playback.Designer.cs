ca399438ef58aa90472a5f76a1a8f73902662078
DeanaLingafelter <Deana.Lingafelter@javs.com>
2017-03-03 11:07:13 -0500

    d

---
+++ MFormatConfidence1/MFormatConfidence1/Playback.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace MFormatConfidence1
+{
+    partial class Playback
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(42, 61);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(548, 263);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(42, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load File";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(42, 356);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(123, 356);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(204, 356);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // Playback
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 432);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.panel1);
+            this.Name = "Playback";
+            this.Text = "Playback";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
---
+++ MFormatConfidence1/MFormatConfidence1/Playback.cs
@@ -0,0 +1,119 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using MCHROMAKEYLib;
+using MFORMATSLib;
+using Microsoft.Win32;
+using MLCHARGENLib;
+using eMFCC = MFORMATSLib.eMFCC;
+using eMFrameClone = MFORMATSLib.eMFrameClone;
+using eMFrameFlags = MFORMATSLib.eMFrameFlags;
+using eMVideoFormat = MFORMATSLib.eMVideoFormat;
+using M_AV_PROPS = MFORMATSLib.M_AV_PROPS;
+using M_TIME = MFORMATSLib.M_TIME;
+namespace MFormatConfidence1
+{
+    public partial class Playback : Form
+    {
+        enum eState
+        {
+            eST_PlayFwd,
+            eST_Pause,
+            eST_PlayRev,
+            eST_StepFwd,
+            eST_StepRev
+        };
+        class PlayerState
+        {
+            public Object stateLock = new Object();
+            public eState state;
+            public double dblFrameRequest;
+            public double dblDuration;
+            public double dblRate;
+            public string strFileName;
+            public bool bLoop;
+        }
+
+        private MFReaderClass objMFReader;            //MFormats Reader object
+        private MFPreviewClass objPreview;            //Preview object
+        private PlayerState playerState;              //Playback state
+        private MFRendererClass objRenderer;          //MRenderer object
+        private MFSinkClass objMFSink;                //MFSink object
+
+        private MFFactory objFactory;			//Frames class used to create MFrame from file
+        private Thread threadWorker;	//Working thread
+        private Object objLock = new Object();
+        private bool isWorking = false;
+        private bool isThreading = false;
+
+        private M_AV_PROPS avProps;
+        private M_TIME Time;
+        private string[] m_arrArgs;
+        private CancellationTokenSource cancelSource;
+
+        public Playback(string[] args)
+        {
+            m_arrArgs = args;
+            InitializeComponent();
+        }
+
+        private void Playback_Load(object sender, EventArgs e)
+        {
+            objFactory = new MFFactory();
+            objPreview = new MFPreviewClass();
+            playerState = new PlayerState();
+            objRenderer = new MFRendererClass();
+            objMFSink = new MFSinkClass();
+
+            playerState.bLoop = checkBoxLoop.Checked;
+
+            // First file start with pause, next file open in same state
+            pause();
+
+            //Configure preview
+            objPreview.PreviewWindowSet("", panelPreview.Handle.ToInt32());
+            objPreview.PreviewEnable("", Convert.ToInt32(checkBoxAudio.Checked), Convert.ToInt32(checkBoxVideo.Checked));
+
+            //FillVideoFormats();
+            avProps.vidProps.eVideoFormat = eMVideoFormat.eMVF_Custom;
+
+            // Fill renderer devices
+            FillRenderers();
+
+            cancelSource = new CancellationTokenSource();
+            threadWorker = new Thread(() => thread_DoWork(cancelSource.Token));
+            threadWorker.Name = "thread_DoWork";
+            threadWorker.Start();
+
+            if (m_arrArgs.Length > 0 && File.Exists(m_arrArgs[0]))
+                OpenFile(m_arrArgs[0]);
+        }
+
+        private void thread_DoWork(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                if (NextFrame() == false) return;
+            }
+        }
+
+        private bool NextFrame()
+        {
+            return true;
+        }
+    }
+}
